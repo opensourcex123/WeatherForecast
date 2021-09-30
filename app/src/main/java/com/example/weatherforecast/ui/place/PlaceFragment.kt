90b8f2e3d9467438b40331144356830928b21f91
nfx_git <13561592791@163.com>
2021-09-30 19:17:20 +0800

    完成初步工作，实现查询地点

---
+++ app/src/main/java/com/example/weatherforecast/ui/place/PlaceFragment.kt
@@ -0,0 +1,76 @@
+package com.example.weatherforecast.ui.place
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherforecast.R
+import com.example.weatherforecast.databinding.FragmentPlaceBinding
+import com.example.weatherforecast.logic.model.Place
+
+// 实现place fragment的类
+class PlaceFragment : Fragment() {
+
+    // 获取place view model的实例
+    val viewModel by lazy { ViewModelProvider(this).get(PlaceViewModel::class.java) }
+
+    private lateinit var adapter: PlaceAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_place, container, false)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val recyclerView: RecyclerView = activity!!.findViewById(R.id.recyclerView)
+        val searchPlaceEdit: EditText = activity!!.findViewById(R.id.searchPlaceEdit)
+        val bgImageView: ImageView = activity!!.findViewById(R.id.bgImageView)
+        val layoutManager = LinearLayoutManager(activity)
+        recyclerView.layoutManager = layoutManager
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        recyclerView.adapter = adapter
+        // 监听搜索框内容的变化
+        searchPlaceEdit?.addTextChangedListener { editable ->
+            val content = editable.toString()
+            if (content.isNotEmpty()) {
+                viewModel.searchPlaces(content)
+            } else {
+                recyclerView.visibility = View.GONE
+                bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+        // 获取服务器响应的数据
+        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer { result ->
+            val places = result.getOrNull()
+            if (places != null) {
+                recyclerView.visibility = View.VISIBLE
+                bgImageView.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+    }
+}
